1b7c812305c1b463b068eb2ca8450cb71a4d7f9b
thijsBoet <m.boet@chello.nl>
2022-07-16 00:29:37 +0200

    bug in search results no styling in buttons

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import Pagination from '@mui/material/Pagination';
+import { Box, Stack, Typography } from '@mui/material';
 
-const Exercises = () => {
+import ExerciseCard from './ExerciseCard';
+
+import { exerciseOptions, fetchData } from '../utils/fetchData';
+
+const Exercises = ({ exercises, setExercises, setBodyPart }) => {
+
 	return (
-		<div>Exercises</div>
-	)
-}
+		<Box id='exercises' sx={{ mt: { lg: '110px' } }} mt='50px' p='20px'>
+			<Typography variant='h3' mb='46px'>
+				Showing Results
+			</Typography>
+			<Stack
+				direction='row'
+				sx={{ gap: { lg: '110px', xs: '50px' } }}
+				flexWrap='wrap'
+				justifyContent='center'
+			>
+				{exercises.map((exercise, index) => (
+					<ExerciseCard exercise={exercise} key={index} />
+				))}
+			</Stack>
+		</Box>
+	);
+};
 
-export default Exercises
+export default Exercises;
