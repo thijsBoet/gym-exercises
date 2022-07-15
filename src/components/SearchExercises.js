49ab9478586cb3cbfbf30dbea8db44fc246c40b2
thijsBoet <m.boet@chello.nl>
2022-07-15 19:28:43 +0200

    finsihed UI SearchExercises component

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,19 +1,66 @@
-import React, { useState, useEffect } from 'react'
-import { Box, Button, Stack, TextField, Typography } from '@mui/material'
+import React, { useState, useEffect } from 'react';
+import { Box, Button, Stack, TextField, Typography } from '@mui/material';
 
 const SearchExercises = () => {
+	const [search, setSearch] = useState('')
+
+	const handleSearch = async (e) => {
+		e.preventDefault();
+		if (search) {
+			const exercisesData = await fetch(`/api/exercises?search=${search}`)
+		}
+	}
+
 	return (
 		<Stack alignItems='center' mt='37px' justifyContent='center' p='20px'>
-			<Typography fontWeight='700'  sx={{fontSize : {lg: '44px', xs: '30px'}}} mb='50px' textAlign='center'>
+			<Typography
+				fontWeight='700'
+				sx={{ fontSize: { lg: '44px', xs: '30px' } }}
+				mb='50px'
+				textAlign='center'
+			>
 				Awesome Exercises You
 				<br />
 				Should Know
 			</Typography>
 			<Box position='relative' mb='72px'>
-				<TextField />
+				<TextField
+					sx={{
+						input: {
+							fontWeight: '700',
+							border: 'none',
+							borderRadius: '4px',
+						},
+						width: { lg: '800px', xs: '350px' },
+						backgroundColor: '#fff',
+						borderRadius: '40px',
+					}}
+					height='76px'
+					value={search}
+					onChange={(e) => setSearch(e.target.value.toLowerCase().trim())}
+					onClick={handleSearch}
+					placeholder='Search Exercises'
+					type='text'
+				/>
+				<Button
+					className='search-btn'
+					sx={{
+						bgcolor: '#FF2625',
+						color: '#fff',
+						textTransform: 'none',
+						width: { lg: '173px', xs: '80px' },
+						height: '56px',
+						position: 'absolute',
+						right: '0px',
+						fontSize: { lg: '20px', xs: '14px' },
+					}}
+					onClick={() => {}}
+				>
+					Search
+				</Button>
 			</Box>
 		</Stack>
 	);
-}
+};
 
-export default SearchExercises
+export default SearchExercises;
